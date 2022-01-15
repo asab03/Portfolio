007fe0e220921907f99cda1f55fc592020f1ec2f
a_sabella@stu-digital-campus.fr <a_sabe>
2022-01-15 17:27:29 +0100

    Add event Js

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ window.addEventListener("scroll", () => {
     retourAccueil.style.top = "-50px";
   }
 });
+const redirectPj1 = document.querySelector(".btnpj1");
+redirectPj1.addEventListener("click", () => {
+  window.open("https://github.com/asab03/Home-Travel");
+});
+
+const redirectPj2 = document.querySelector(".btnpj2");
+redirectPj2.addEventListener("click", () => {
+  window.open("https://github.com/asab03/shortcuts-API");
+});
+
+const redirectPj3 = document.querySelector(".btnpj3");
+redirectPj3.addEventListener("click", () => {
+  window.open("https://github.com/asab03/todolist");
+});
+
+const redirectPj1T = document.querySelector(".btn1");
+redirectPj1T.addEventListener("touchstart", () => {
+  window.open("https://github.com/asab03/Home-Travel");
+});
+const redirectPj2T = document.querySelector(".btn2");
+redirectPj2T.addEventListener("touchstart", () => {
+  window.open("https://github.com/asab03/shortcuts-API");
+});
+const redirectPj3T = document.querySelector(".btn3");
+redirectPj3T.addEventListener("touchstart", () => {
+  window.open("https://github.com/asab03/todolist");
+});
